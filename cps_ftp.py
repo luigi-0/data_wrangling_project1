43ff0015e96c37122230fd7f4119a15d745bf0e4
Luis Granados <granal88@gmail.com>
2019-10-18 12:28:05 -0400

    cleaned up the code a bit

--- cps_ftp.py
+++ cps_ftp.py
@@ -5,19 +5,17 @@ Created on Wed Oct 16 19:35:02 2019
 
 @author: luisgranados
 """
-import os
 import requests
 import re
 from bs4 import BeautifulSoup
 
-os.chdir("/Users/luisgranados/Documents/R-Projects/R-for_data_science/parsing/codebooks")
-
-# This block of code works for getting all the links to the dictionaries
 def cps_links(filetype):
     """
     Find and store the desired links from the Census' FTP site.
     
     Parameters (character): The type of file you want to be stored(.txt, .zip, ect.).
+    
+    Returns (list): A list containing all the links the selected file types 
     """
     url = "https://thedataweb.rm.census.gov/ftp/cps_ftp.html"
     response = requests.get(url)
@@ -30,16 +28,13 @@ def cps_links(filetype):
             
     return links
 
-# This will work for downloading all the data dictionaries text files
 def codebook_downloader(links):
-    """Download all the text files from the Census' FTP site."""
+    """
+    Download all the selected files from the Census' FTP site.
+    
+    Parameters (list): A list containing all the links for the files
+    """
     for link in links:
         file = requests.get(link, allow_redirects=True)
         filename = re.search(r"(?<=[/])[\w\d_.-]+(.txt|.zip)$", link).group(0)
         open(filename, 'wb').write(file.content)
-
-
-os.chdir("/Users/luisgranados/Documents/R-Projects/R-for_data_science/parsing/datafiles")
-
-data = cps_links(".zip")
-codebook_downloader(data)
