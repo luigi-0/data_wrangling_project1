5e626bce9305582b173e2269453c80ea8acbf4c9
Luis Granados <granal88@gmail.com>
2019-11-02 18:00:03 -0400

    renamed df to dataframe

--- scripts/labstat_downloader.py
+++ scripts/labstat_downloader.py
@@ -19,40 +19,40 @@ def labstat_importer(url, series):
     Returns:
         dataframe
     """
-    df = pd.read_csv(url, sep="\t", low_memory=False)
+    dataframe = pd.read_csv(url, sep="\t", low_memory=False)
 
-    df.rename(str.strip, axis='columns', inplace=True)
+    dataframe.rename(str.strip, axis='columns', inplace=True)
 
-    df['series_id'] = df['series_id'].str.strip()
+    dataframe['series_id'] = dataframe['series_id'].str.strip()
 
-    df = df.loc[df["series_id"].isin(series)]
+    dataframe = dataframe.loc[dataframe["series_id"].isin(series)]
 
-    return df
+    return dataframe
 
-def lab_monthly(df):
+def lab_monthly(dataframe):
     """
     Extract monthly data from a labstat file
 
     Parameters:
-        df (dataframe): A dataframe containing labstat data
+        dataframe (dataframe): A dataframe containing labstat data
 
     Returns:
         dataframe
     """
-    df['month'] = df['period'].str[1:].copy().astype(int)
+    dataframe['month'] = dataframe['period'].str[1:].copy().astype(int)
 
-    df['value'] = df['value'].astype(int)
+    dataframe['value'] = dataframe['value'].astype(int)
 
-    df = df.loc[~df['month'].isin([13])]
+    dataframe = dataframe.loc[~dataframe['month'].isin([13])]
 
-    return df
+    return dataframe
 
-def select_month(df, year, month):
+def select_month(dataframe, year, month):
     """
     Extract specific year and month from labstat file
 
     Parameters:
-        df (dataframe): A dataframe containing labstat data
+        dataframe (dataframe): A dataframe containing labstat data
 
         Year (int): The year you want to select
 
@@ -61,27 +61,27 @@ def select_month(df, year, month):
     Returns:
         dataframe
     """
-    df = df.loc[(df['year'] == year) & \
-              (df['month'] == month)]
+    dataframe = dataframe.loc[(dataframe['year'] == year) & \
+              (dataframe['month'] == month)]
 
-    return df
+    return dataframe
 
-def recent_month(df):
+def recent_month(dataframe):
     """
     Retrieve the most recent month of data
 
     Parameters:
-        df (dataframe): A dataframe containing labstat data
+        dataframe (dataframe): A dataframe containing labstat data
 
     Returns:
         dataframe
     """
-    current_period = df.sort_values(by=['year', 'month']).tail(1)[['year', 'month']]
+    current_period = dataframe.sort_values(by=['year', 'month']).tail(1)[['year', 'month']]
 
     year = current_period['year'].values[0]
 
     month = current_period['month'].values[0]
 
-    df = select_month(df, year, month)
+    dataframe = select_month(dataframe, year, month)
 
-    return df
+    return dataframe
