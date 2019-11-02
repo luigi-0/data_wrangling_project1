a052f07f8505ea0ca8aba477488253e485c8c27b
Luis Granados <granal88@gmail.com>
2019-11-02 12:43:44 -0400

    removed trailing whitespace

--- scripts/labstat_downloader.py
+++ scripts/labstat_downloader.py
@@ -11,38 +11,38 @@ import pandas as pd
 def labstat_importer(url, series):
     """
     Import data from the BLS labstat files
-    
+
     Parameters:
         url (string): URL of labstat file
         series (list): BLS series codes
-        
+
     Returns:
         dataframe
     """
     df = pd.read_csv(url, sep="\t", low_memory=False)
-    
+
     df.rename(str.strip, axis='columns', inplace=True)
-    
+
     df['series_id'] = df['series_id'].str.strip()
-    
+
     df = df.loc[df["series_id"].isin(series)]
-    
+
     return df
 
 def lab_monthly(df):
     """
     Extract monthly data from a labstat file
-    
+
     Parameters:
         df (dataframe): A dataframe containing labstat data
-        
+
     Returns:
         dataframe
     """
     df['month'] = df['period'].str[1:].copy().astype(int)
 
     df['value'] = df['value'].astype(int)
-    
+
     df = df.loc[~df['month'].isin([13])]
 
     return df
@@ -50,38 +50,38 @@ def lab_monthly(df):
 def select_month(df, year, month):
     """
     Extract specific year and month from labstat file
-    
+
     Parameters:
         df (dataframe): A dataframe containing labstat data
-        
+
         Year (int): The year you want to select
-        
+
         Month (int): The month you want to select
-        
+
     Returns:
         dataframe
     """
     df = df.loc[(df['year'] == year) & \
               (df['month'] == month)]
-    
+
     return df
 
 def recent_month(df):
     """
     Retrieve the most recent month of data
-    
+
     Parameters:
         df (dataframe): A dataframe containing labstat data
-        
+
     Returns:
         dataframe
     """
     current_period = df.sort_values(by=['year', 'month']).tail(1)[['year', 'month']]
-    
+
     year = current_period['year'].values[0]
-    
+
     month = current_period['month'].values[0]
-    
+
     df = select_month(df, year, month)
-    
+
     return df
