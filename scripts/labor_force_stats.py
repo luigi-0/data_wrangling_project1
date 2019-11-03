d8a7f6c6e6aaa4712f0479d02ef2c81fe89bb860
Luis Granados <granal88@gmail.com>
2019-11-03 18:46:21 -0500

    refactored the load of the dataframe

--- scripts/labor_force_stats.py
+++ scripts/labor_force_stats.py
@@ -6,28 +6,9 @@ Created on Wed Oct 23 20:21:28 2019
 @author: luisgranados
 """
 
-import pandas as pd
 import cps_ftp as cf
 
-# CPS public file to be imported
-CPS_FILE = 'sep19pub.zip'
-
-# Read in the CPS variables you are interested in
-cf.path_finder('settings')
-CPS_VARS = pd.read_csv('CPS_selected_variables')
-
-# Import the parsed CPS codebook
-CODEBOOK = cf.parsed_codebook_importer('January_2017_Record_Layout_parsed')
-
-# Store the location of the selected CPS variables
-FIELDS = CPS_VARS.merge(CODEBOOK)
-
-COLSPECS = cf.location_parser(FIELDS, 'LOCATION')
-COLSPECS = cf.location_modifier(COLSPECS)
-
-# Import the CPS public file
-cf.path_finder('datafiles')
-DATAFRAME = pd.read_fwf(CPS_FILE, colspecs=COLSPECS, names=FIELDS.NAME, na_values=[-1])
+DATAFRAME = cf.cps_data_importer('January_2017_Record_Layout_parsed', 'sep19pub.zip')
 
 # Store the year and month of the imported CPS datafile
 FILE_YEAR = DATAFRAME['HRYEAR4'].values[0]
