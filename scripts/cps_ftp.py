386091ed6bcbce541d34f1e620a7f0b749f2fb07
Luis Granados <granal88@gmail.com>
2019-11-03 16:29:38 -0500

    cleaned up some code and syntax

--- scripts/cps_ftp.py
+++ scripts/cps_ftp.py
@@ -6,15 +6,15 @@ Created on Wed Oct 16 19:35:02 2019
 @author: luisgranados
 """
 import os
-import requests
 import re
+import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
 def path_finder(sub):
     """
     Set working directory to the desired path
-    
+
     Arguments:
         sub (character): The name of the directory you want to visit
     """
@@ -26,52 +26,52 @@ def path_finder(sub):
 def link_crawler(url, filetype):
     """
     Find and store the desired links from a site.
-    
+
     Parameters:
         url (character): The url to the website
         filetype (character): The type of file you want to be stored(.txt, .zip, ect.)
-    
-    Returns (list): A list containing all the links the selected file types 
+
+    Returns (list): A list containing all the links the selected file types
     """
     response = requests.get(url)
     soup = BeautifulSoup(response.content, features="lxml")
-    
+
     links = []
     for link in soup.find_all('a', href=True):
         if link['href'].lower().endswith(filetype):
             links.append(link['href'])
-            
+
     return links
 
 def cps_ftp_links(filetype):
     """
     Find the links in the Basic Monthly File table.
-    
+
     Parameters:
         filetype (character): The type of file you want to be stored(.txt, .zip, ect.)
-    
+
     Returns (list): A list containing all the links the selected file types
     """
     url = "https://thedataweb.rm.census.gov/ftp/cps_ftp.html"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, features="lxml")
-    
-    links = []   
+
+    links = []
     for table in soup.find_all('table'):
         if table.b.text == "Basic Monthly CPS4":
             for link in table.find_all('a', href=True):
                 if link['href'].lower().endswith(filetype):
                     links.append(link['href'])
-                    
+
     return links
 
 def pub_filename(filetype):
     """
     Regex pattern to search for the public use CPS filename.
-    
+
     Parameters:
         filetype (character): The type of file you want to search for (.txt, .zip, ect.)
-        
+
     Returns (character): The regex pattern required to find the desired filetype
     """
     if filetype == ".txt":
@@ -82,16 +82,16 @@ def pub_filename(filetype):
 def new_files(filetype):
     """
     Find new files to download from the Census' FTP webpage.
-    
+
     Parameters:
         filetype (character): The type of file you want to search for (.txt, .zip, ect.)
-        
+
     Returns (list): List of files not found in local directory
     """
     pattern = pub_filename(filetype)
     pub_links = cps_ftp_links(filetype)
     present = os.listdir()
-    
+
     pub_files = {}
     for file in pub_links:
         if re.search(pattern, file):
@@ -102,20 +102,20 @@ def new_files(filetype):
     for file in pub_files:
         if file not in present:
             missing_file.append(pub_files[file])
-    
+
     return missing_file
 
 def file_downloader(filetype):
     """
     Download all the selected files from the Census' FTP site.
-    
+
     Parameters :
         links (character): Links containing the files to be downloaded
 
     """
     links = new_files(filetype)
     pattern = pub_filename(filetype)
-    
+
     for link in links:
         file = requests.get(link, allow_redirects=True)
         if re.search(pattern, link):
@@ -125,13 +125,13 @@ def file_downloader(filetype):
 def file_parser(source, parsed):
     """
     Parse the CPS codebook and standardize the layout of the file.
-    
+
     Parameters:
         source (character): The name of the CPS codebook file
-        
+
         parsed (character): The name of the parsed output file
     """
-    with open(source, encoding = 'cp1252') as data_dict:
+    with open(source, encoding='cp1252') as data_dict:
         with open(parsed, "w") as f:
             for line in data_dict:
                 # Collapse more than two space into no space
@@ -146,29 +146,31 @@ def file_parser(source, parsed):
                 line = re.sub(r"(?<=[-])[\t ]|[\t ](?=[-])", "", line, flags=re.IGNORECASE)
                 # Remove tabs at end of line
                 line = re.sub(r"[\t]$", "", line, flags=re.IGNORECASE)
-                if re.search("(NAME)[\t ]+(SIZE)[\t ]+(DESCRIPTION)[\t ]+(LOCATION)", line, flags=re.IGNORECASE):
+                if re.search("(NAME)[\t ]+(SIZE)[\t ]+(DESCRIPTION)[\t ]+(LOCATION)",
+                             line, flags=re.IGNORECASE):
                     f.write(line)
                 elif re.search(r"^(FILLER|PADDING)[\t][\d][\t][\d-]+", line, flags=re.IGNORECASE):
                     line = re.sub(r"(?<=[\d])[\t](?=[\d])", "\tNA\t", line, flags=re.IGNORECASE)
                     f.write(line)
                 #This finds the identifier information
-                elif re.search("^[\w\d]+[\t][\d]+[\t][\w\d\W\D ]+[\t][\d]+[ \D\W]+[\d]+", line, flags=re.IGNORECASE):
+                elif re.search(r"^[\w\d]+[\t][\d]+[\t][\w\d\W\D ]+[\t][\d]+[ \D\W]+[\d]+",
+                               line, flags=re.IGNORECASE):
                     # Remove tabs inside the description column
                     line = re.sub(r"(?<=[A-z])[\t](?=[A-z\D\W])", " ", line, flags=re.IGNORECASE)
                     f.write(line)
                 else:
                     line = re.sub(r"^[\t ]{1,}", "", line, flags=re.IGNORECASE)
                     line = re.sub(r"[\t]", " ", line, flags=re.IGNORECASE)
-                    line = re.sub(r"^[\t ]{0,}", "NA\tNA\t", line, flags=re.IGNORECASE)        
+                    line = re.sub(r"^[\t ]{0,}", "NA\tNA\t", line, flags=re.IGNORECASE)
                     f.write(line)
 
 def row_skipper(file):
     """
     Count how many rows are needed to be skipped in the parsed codebook.
-    
+
     Parameters:
         file (character): File name of the parsed codebook
-        
+
     Returns:
         count: The number of lines to be skipped
     """
@@ -184,16 +186,16 @@ def row_skipper(file):
 def location_parser(selected_variables, column):
     """
     Parse the location variable by creating a list of tuples.
-    
+
     Remove the hyphen between the start/stop positions. Convert all elements to
     integers and create a list of tuples.
-    
+
     Parameters:
         selected_variables (dataframe): The dataframe containing the location of
         the variables contained in the cps_selected_variables file
-        
+
         column (character): The name of the column containing the start/stop positions
-    
+
     Returns:
         selected_fields: A list of tuples containing the start/stop positions
     """
@@ -207,12 +209,12 @@ def location_parser(selected_variables, column):
 def location_modifier(fields):
     """
     Modify the parsed location variable.
-    
+
     Subtract 1 from all the start positions in order to be able to use pd.read_fwf()
-    
+
     Parameters:
         field (list): The list of tuples created by location_parser()
-    
+
     Returns:
         fields: A list of tuples
     """
@@ -224,10 +226,10 @@ def location_modifier(fields):
 def parsed_codebook_importer(codebook):
     """
     Import the parsed CPS codebook
-    
+
     Parameters:
         codebook (str): the filename of the parsed codebook
-        
+
     Returns:
         dataframe
     """
@@ -235,7 +237,7 @@ def parsed_codebook_importer(codebook):
     skip = row_skipper(codebook)
     codebook = pd.read_csv(codebook, sep="\t", skiprows=skip).dropna()
     os.chdir('..')
-    
+
     return codebook
 
 def cps_data_importer(codebook, datafile):
